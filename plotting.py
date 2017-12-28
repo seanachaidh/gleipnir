7c2c0238cefe584e7470ebbe00522f614c2307a6
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2017-12-28 23:21:23 +0100

    code update

--- plotting.py
+++ plotting.py
@@ -2,40 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class PlotElement:
-    def __init__(self, title, values):
-        self.title = title
-        self.values = values
+	def __init__(self, title, values):
+		self.title = title
+		self.values = values
 
 class Plot:
-    def __init__(self, title, xlabel, ylabel, savefile = None):
-        self.value_list = []
-        self.title = title
-        self.xlabel = xlabel
-        self.ylabel = ylabel
-        self.savefile = savefile
-        
-    def plot(self):
-        raise NotImplementedError('you must implement this method')
+	def __init__(self, title, xlabel, ylabel, savefile = None):
+		self.value_list = []
+		self.title = title
+		self.xlabel = xlabel
+		self.ylabel = ylabel
+		self.savefile = savefile
+	
+	def append_values(self, title, values, pre_proccess = False):
+		if pre_proccess:
+			newvals = list(map(lambda x: round(x, 5), values))
+		else:
+			newvals = values
+		self.value_list.append(PlotElement(title, newvals))
+	
+	def plot(self):
+		raise NotImplementedError('you must implement this method')
 
 class LinePlot(Plot):
-    def __init__(self, title, xlabel, ylabel, savefile = None, legend = False):
-        super(LinePlot, self).__init__(title, xlabel, ylabel, savefile)
-        self.legend = legend
-    def plot(self):
-        fig = plt.figure()
-        ran = max([len(v.values) for v in self.value_list])
-        xran = np.arange(ran)
-        
-        for v in self.value_list:
-            plt.plot(xran, v.values, label = v.title)
-        
-        plt.title(self.title)
-        plt.xlabel(self.xlabel)
-        plt.ylabel(self.ylabel)
-        
-        if self.legend:
-            plt.legend()
-        if not self.savefile is None:
-            fig.savefig(self.savefile)
-        else:
-            plt.show()
+	def __init__(self, title, xlabel, ylabel, savefile = None, legend = False):
+		super(LinePlot, self).__init__(title, xlabel, ylabel, savefile)
+		self.legend = legend
+	def plot(self):
+		fig = plt.figure()
+		ran = max([len(v.values) for v in self.value_list])
+		xran = np.arange(ran)
+		
+		for v in self.value_list:
+			plt.plot(xran, v.values, label = v.title)
+		
+		plt.title(self.title)
+		plt.xlabel(self.xlabel)
+		plt.ylabel(self.ylabel)
+		
+		if self.legend:
+			plt.legend()
+		if not self.savefile is None:
+			fig.savefig(self.savefile)
+		else:
+			plt.show()
--- test_matrixgame.py
+++ test_matrixgame.py
@@ -13,21 +13,23 @@ game.set_qvalue(1,2,-1,None)
 game.set_qvalue(0,2,1,None)
 game.set_qvalue(2,0,-1,None)
 
-game.add_players(gleipnir.PHCPlayer(3, 1, 0, 0.2, 0.0, 0.001), gleipnir.PHCPlayer(3, 1, 0, 0.2, 0.0, 0.001))
+game.add_players(gleipnir.PHCPlayer(3, 1, 0, 0.2, 0.0, 0.2), gleipnir.PHCPlayer(3, 1, 0, 0.2, 0.0, 0.2))
 game.play_n_games(1000)
 
 lineplot = plotting.LinePlot('Action selection probability', 'Probability', 'Games', 'matrixgamephc1.png', True)
-lineplot.value_list.append(plotting.PlotElement('action1', game.player1_stats[0]))
-lineplot.value_list.append(plotting.PlotElement('action2', game.player1_stats[1]))
-lineplot.value_list.append(plotting.PlotElement('action3', game.player1_stats[2]))
+lineplot.append_values('action1', game.player1_stats[0], True)
+lineplot.append_values('action2', game.player1_stats[1], True)
+lineplot.append_values('action3', game.player1_stats[2], True)
 
 lineplot.plot()
 
-lineplot2 = lineplot = plotting.LinePlot('Action selection probability', 'Probability', 'Games', 'matrixgamephc2.png', True)
-lineplot2.value_list.append(plotting.PlotElement('action1', game.player2_stats[0]))
-lineplot2.value_list.append(plotting.PlotElement('action2', game.player2_stats[1]))
-lineplot2.value_list.append(plotting.PlotElement('action3', game.player2_stats[2]))
+lineplot2 = plotting.LinePlot('Action selection probability', 'Probability', 'Games', 'matrixgamephc2.png', True)
+lineplot2.append_values('action1', game.player2_stats[0], True)
+lineplot2.append_values('action2', game.player2_stats[1], True)
+lineplot2.append_values('action3', game.player2_stats[2], True)
 lineplot2.plot()
+
+print(game.player2_stats[0])
 
 
 wolfgame = gleipnir.MatrixGame(1, 3)
@@ -42,7 +44,7 @@ wolfgame.set_qvalue(0,2,1,None)
 wolfgame.set_qvalue(2,0,-1,None)
 
 wolfgame.add_players(gleipnir.WolfPlayer(3, 1, 0, 0.2, 0.0, 0.2, 0.4), gleipnir.WolfPlayer(3, 1, 0, 0.2, 0.0, 0.2, 0.4))
-wolfgame.play_n_games(10)
+wolfgame.play_n_games(1000)
 
 wolflineplot = plotting.LinePlot('Action selection probability', 'Probability', 'Games', 'wolf_matrixgamephc1.png', True)
 wolflineplot.value_list.append(plotting.PlotElement('action1', wolfgame.player1_stats[0]))
@@ -56,11 +58,11 @@ wolflineplot2.value_list.append(plotting.PlotElement('action2', wolfgame.player2
 wolflineplot2.value_list.append(plotting.PlotElement('action3', wolfgame.player2_stats[2]))
 wolflineplot2.plot()
 
-#for debugging
-print("wolfgame stats action 1")
-print(wolfgame.player2_stats[0])
-print("wolfgame stats action 2")
-print(wolfgame.player2_stats[1])
-print("wolfgame stats action 3")
-print(wolfgame.player2_stats[2])
+##for debugging
+#print("wolfgame stats action 1")
+#print(wolfgame.player2_stats[0])
+#print("wolfgame stats action 2")
+#print(wolfgame.player2_stats[1])
+#print("wolfgame stats action 3")
+#print(wolfgame.player2_stats[2])
 
